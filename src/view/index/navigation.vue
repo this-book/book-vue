b1b2cd71001c60d41b822f6fda56730814069abb
qian-59 <1846629173@qq.com>
2020-08-28 10:41:30 +0800

    8月28号

--- src/view/index/navigation.vue
+++ src/view/index/navigation.vue
@@ -3,26 +3,34 @@
   <div>
     <div class="col-lg-14" style="width: 100%;position: fixed;top: 0px;z-index: 99999;">
       <ul class="layui-nav">
-        <li class="layui-nav-item"><a href="">最新活动</a></li>
+        <li class="layui-nav-item"><a href="">首页</a></li>
         <li class="layui-nav-item layui-this">
-          <a href="javascript:;">产品</a>
+          <a href="javascript:;">分类</a>
           <dl class="layui-nav-child">
-            <dd><a>选项1</a></dd>
-            <dd><a>选项2</a></dd>
-            <dd><a>选项3</a></dd>
+            <dd><a>古代</a></dd>
+            <dd><a>言情</a></dd>
+            <dd><a>武侠</a></dd>
+            <dd><a>仙侠</a></dd>
+            <dd><a>灵异</a></dd>
+            <dd><a>恐怖</a></dd>
+            <dd><a>玄幻</a></dd>
+            <dd><a>奇幻</a></dd>
+            <dd><a>穿越</a></dd>
+            <dd><a>都市</a></dd>
+            <dd><a>其他书籍</a></dd>
           </dl>
         </li>
-        <li class="layui-nav-item"><a>大数据</a></li>
+        <li class="layui-nav-item"><a>免费</a></li>
         <li class="layui-nav-item">
-          <a href="javascript:;">解决方案</a>
-          <dl class="layui-nav-child">
-            <dd><a>移动模块</a></dd>
-            <dd><a>后台模版</a></dd>
-            <dd class="layui-this"><a>选中项</a></dd>
-            <dd><a>电商平台</a></dd>
-          </dl>
+          <a href="javascript:;">排行</a>
+                    <dl class="layui-nav-child">
+                      <dd><a>年度排行</a></dd>
+                      <dd><a>月度排行</a></dd>
+                      <dd><a>7天排行</a></dd>
+                      <dd><a>一天排行</a></dd>
+                    </dl>
         </li>
-        <li class="layui-nav-item"><a>社区</a></li>
+        <li class="layui-nav-item"><a>完结</a></li>
       </ul>
     </div>
     <br><br>
